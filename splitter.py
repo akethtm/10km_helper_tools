9a08e76bd761ef579dd5bf18b8e14a8ffbcf7aff
Your Name <you@example.com>
2016-10-14 14:33:12 +0530

    finsihed with python modules

--- splitter.py
+++ splitter.py
@@ -6,7 +6,7 @@ lon_dict = {}
 
 for line in content:
         items = line.split()
-        key, values = items[0], items[1:]
+        key, values = items[0], items[1]
         lon_dict[key] = values
 
 
@@ -16,7 +16,7 @@ lat_dict = {}
 
 for line in content2:
         items = line.split()
-        key, values = items[0], items[1:]
+        key, values = items[0], items[1]
         lat_dict[key] = values
 
 islands_data = [line.rstrip('\n') for line in open('islands_data')]
@@ -24,4 +24,7 @@ islands_data = [line.rstrip('\n') for line in open('islands_data')]
 for line in islands_data:
         items = line.split()
         lat, lon1, lon2 = items[0], items[1], items[2]
-        print lat_dict[lat],lon_dict[lon1],lon_dict[lon2]
+        v1 = lat_dict[lat]
+        v2 = lon_dict[lon1] 
+        v3 = lon_dict[lon2]
+        print "%d:%d ,%d\t,0" %(int(v2),int(v3),int(v1))
